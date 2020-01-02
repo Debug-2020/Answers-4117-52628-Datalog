642bc7dbae7c7af4c9b14e1ed588f367a53ec8c2
Debug-2020 <debug2020@protonmail.com>
2020-01-02 15:40:06 +0800

    4117-52628-Datalog-result1575172450548

--- src/Datalog.java
+++ src/Datalog.java
@@ -26,7 +26,7 @@ public class Datalog {
 	}
 
 	public boolean equals(Object o) {
-		if (o == null)
+		if (arguments.length == 0 || predicate.toString() == null)
 			return false;
 		if (o instanceof Datalog) {
 			return ((Datalog) o).predicate.equals(this.predicate)
